2e7d44aa46639b4d88fcf5a4c9556d160f5a9403
duducamargo <edu.farias.camargo@gmail.com>
2024-09-08 17:37:01 -0300

    [add] register task and edit task page frontend has been finalized

--- gerenciador_de_tarefas/gerenciador_de_tarefas/urls.py
+++ gerenciador_de_tarefas/gerenciador_de_tarefas/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', CadastroViews.login, name='login'),
     path('cadastro/', CadastroViews.cadastro, name='cadastro'),
-    path('tasks', TaskViews.tasks, name='tasks')
+    path('tasks/', TaskViews.tasks, name='tasks'),
+    path('nova_task/', TaskViews.nova_task, name='nova_task'),
+    path('editar_task/', TaskViews.editar_task, name='editar_task'),
 ]
--- gerenciador_de_tarefas/tasks/views.py
+++ gerenciador_de_tarefas/tasks/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 def tasks(request):
     return render(request, 'tasks/tasks.html')
 
+def nova_task(request):
+    if request.method == "POST":
+            return render(request, "tasks/tasks.html")
+        
+    return render(request, 'tasks/nova_task.html')
+
+def editar_task(request):
+    if request.method == "POST":
+            return render(request, "tasks/tasks.html")
+    
+    return render(request, 'tasks/editar_task.html') 
