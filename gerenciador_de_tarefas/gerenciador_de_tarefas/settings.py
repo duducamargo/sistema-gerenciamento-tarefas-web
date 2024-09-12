b0a9cdbc8717d0683f76643a432b7e726ce2ddfa
duducamargo <edu.farias.camargo@gmail.com>
2024-09-12 00:39:02 -0300

    [fix] task date has been fixed

--- gerenciador_de_tarefas/gerenciador_de_tarefas/settings.py
+++ gerenciador_de_tarefas/gerenciador_de_tarefas/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = "UTC"
 
