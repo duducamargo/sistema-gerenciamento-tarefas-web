e5a2b010863a96a6857f13287f226d69d7df4b78
duducamargo <edu.farias.camargo@gmail.com>
2024-09-28 17:04:39 -0300

    [add] profile pictures and email validation has been added

--- gerenciador_de_tarefas/cadastro/views.py
+++ gerenciador_de_tarefas/cadastro/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_protect
 from .models import Usuario
 from django.contrib.auth.hashers import check_password, make_password
 from django.contrib import messages
+import random
 
 def login_required(view_func):
     def wrapper(request, *args, **kwargs):
@@ -32,16 +33,42 @@ def login(request):
 
     return render(request, "cadastro/login.html")
 
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from .models import Usuario  # Certifique-se de importar o modelo correto
+from django.views.decorators.csrf import csrf_protect
+import random
+
 @csrf_protect
 def cadastro(request):
+    numero_aleatorio = random.randint(1, 3)
+
     if request.method == "POST":
+        email = request.POST.get("email")
+        
+        if Usuario.objects.filter(email=email).exists():
+            messages.error(request, "Este e-mail já está cadastrado.")
+            return render(request, "cadastro/cadastro.html")
+
         novo_usuario = Usuario()
         novo_usuario.primeiro_nome = request.POST.get("primeiro_nome")
         novo_usuario.ultimo_nome = request.POST.get("ultimo_nome")
-        novo_usuario.email = request.POST.get("email")
-        novo_usuario.senha = make_password(request.POST.get("senha")) 
+        novo_usuario.email = email
+        novo_usuario.senha = make_password(request.POST.get("senha"))
+
+        match numero_aleatorio:
+            case 1:
+                novo_usuario.foto_perfil = "foto_perfil_1.png"
+            case 2:
+                novo_usuario.foto_perfil = "polar-bear.png"
+            case 3:
+                novo_usuario.foto_perfil = "reindeer.png"
+            case _:
+                novo_usuario.foto_perfil = "foto_perfil_1.png"
+
         novo_usuario.save()
 
-        return redirect("login")  
+        return redirect("login")
 
     return render(request, "cadastro/cadastro.html")
